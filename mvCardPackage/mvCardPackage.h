25cff40f31075a43ba8156218c1caf595ae9bb90
Krunal Chaudhari <krunal.c@avdevs.com>
2020-12-18 11:31:07 +0530

    Initial Commit

---
+++ mvCardPackage/mvCardPackage.h
@@ -0,0 +1,18 @@
+//
+//  mvCardPackage.h
+//  mvCardPackage
+//
+//  Created by Apple on 18/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mvCardPackage.
+FOUNDATION_EXPORT double mvCardPackageVersionNumber;
+
+//! Project version string for mvCardPackage.
+FOUNDATION_EXPORT const unsigned char mvCardPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mvCardPackage/PublicHeader.h>
+
+
